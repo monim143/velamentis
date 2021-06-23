0fd8d59b5f37228e3d0ad8d0562db5c618469b86
M A Monim <ma.monim35@gmail.com>
2021-06-23 18:03:18 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+/**
+ * @Script js for (Template/Project Name)
+ *
+ * @project     - Project Name
+ * @author      - 
+ * @created_by  - 
+ * @created_at  - 
+ * @modified_by -
+ */
+
+
+/**
+ * ========================================================
+ * this function execute when window properly loaded
+ * ===========================================================
+ */
+
+$(window).on('load', function () {
+
+    // code should be execute here
+
+});
+
+
+
+/**
+ * ========================================================
+ * this function execute when DOM element ready 
+ * ===========================================================
+ */
+
+$(document).ready(function () {
+
+    // toggle menu
+    $(function () {
+        $('.navbar-toggler').length &&
+            $('.navbar-toggler').on('click', function () {
+                $(this).find('.ni').toggleClass('ni-menu ni-cross')
+            })
+    });
+    
+});
